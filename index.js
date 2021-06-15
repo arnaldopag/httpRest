a95b730b8bdccdfef8fd691e2b4f3c46515c9900
Arnaldo Pagani <arnaldopaganijr@gmail.com>
2021-06-15 15:52:32 -0300

    meu deus

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const url = fetch('https://api-ppw.herokuapp.com/api/103025/users')
 const botao = document.querySelector('button')
 botao.addEventListener('click', mostrar)
+const ul = document.querySelector('#lista')
 var lista = []
 
 var dado = url.then(function(resposta){
@@ -11,12 +12,11 @@ dado.then(function(nome){
     return console.log((nome))
 })
 
-function mostrar(){
-const nome = document.querySelector('#nome')
-const senha = document.querySelector('#senha');
-var n = nome.value
-var s = senha.value
+function mostrarUsuario(){
+for(const usuario of lista){
+    const li = document.createElement('li')
+    li.textContent = `${usuario.name} (${usuario.password})`
+    ul.appendChild(li)
 
-console.log(n)
-console.log(s)
+}
 }
